00fe23ab47e7ac9a1de4279f396478554006b392
AGoel894 <aadi.goel2009@gmail.com>
2025-09-10 16:37:47 -0700

    Update homepage.py

--- frontend/homepage.py
+++ frontend/homepage.py
@@ -3,6 +3,13 @@ import streamlit as st
 st.title("knowt")
 st.subheader("slogan or fact")
 
+
+with col1:
+    st.button("Generate real practice tests with answer keys and explanations.")
+with col2:
+    st.button("Access study guides")
+
+
 if st.button("Generate real practice tests with answer keys and explanations."):
     pass
 
@@ -17,3 +24,5 @@ ap_exams = [
 
 for exam in ap_exams:
     st.button(exam)
+
+with col2:
