432387963b1a046e26a731a02801d3eb05acfd59
AGoel894 <aadi.goel2009@gmail.com>
2025-09-10 16:40:52 -0700

    Update homepage.py

--- frontend/homepage.py
+++ frontend/homepage.py
@@ -1,16 +1,18 @@
 import streamlit as st
 
-st.title("knowt")
-st.subheader("slogan or fact")
+col1, col2, col3 = st.columns([1, 2, 1])
 
-
-with col1:
-    st.button("Generate real practice tests with answer keys and explanations.")
-
+col4, col5 = st.columns([1, 1])
 
 with col2:
-    st.button("Access study guides")
+    st.title("knowt")
+    st.subheader("slogan or fact")
 
+with col4:
+    st.button("Generate real practice tests with answer keys and explanations.")
+
+with col5:
+    st.button("Access study guides")
 
 if st.button("Generate real practice tests with answer keys and explanations."):
     pass
@@ -26,5 +28,3 @@ ap_exams = [
 
 for exam in ap_exams:
     st.button(exam)
-
-with col2:
