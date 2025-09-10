32d8bfc08b3e0a522f86ec72b2882eafd29e1e55
AGoel894 <aadi.goel2009@gmail.com>
2025-09-10 16:44:21 -0700

    Update homepage.py

--- frontend/homepage.py
+++ frontend/homepage.py
@@ -4,17 +4,29 @@ col1, col2, col3 = st.columns([1, 2, 1])
 
 col4, col5 = st.columns([1, 1])
 
+col6, col7, col8 = st.columns([1, 1, 1])
+
+with col6:
+    st.button("Home")
+
+with col7:
+    st.button("Practice Tests")
+
+with col8:
+    st.button("Study Guides")
+
 with col2:
-    st.title("knowt")
-    st.subheader("slogan or fact")
+    st.title("App Name")
+    st.subheader("slogan")
 
 with col4:
-    st.button("Generate real practice tests with answer keys and explanations.")
+    st.button("Generate Practice Tests")
 
 with col5:
-    st.button("Access study guides")
+    st.button("Access Study Guides")
 
-st.subheader("Available AP Exams:")
+with col2: 
+    st.subheader("Available AP Exams:")
 
 ap_exams = [
     "AP Chemistry"
