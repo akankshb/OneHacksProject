6a3000a5a701844acffa046108eadf7b655ab269
AGoel894 <aadi.goel2009@gmail.com>
2025-09-10 17:00:56 -0700

    Update homepage.py

--- frontend/homepage.py
+++ frontend/homepage.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 if "page" not in st.session_state:
     st.session_state.page = "Home"
+    
+col6, col7, col8 = st.columns([1, 1, 1])
 
 col1, col2, col3 = st.columns([1, 2, 1])
 
 col4, col5 = st.columns([1, 1])
 
-col6, col7, col8 = st.columns([1, 1, 1])
 
 with col6:
     if st.button("Home", use_container_width=True):
@@ -23,8 +24,8 @@ with col8:
 
 if st.session_state.page == "Home":
     with col2:
-        st.title("App Name", use_container_width=True)
-        st.subheader("slogan", use_container_width=True)
+        st.title("App Name")
+        st.subheader("slogan")
 
     with col4:
         if st.button("Generate Practice Tests", use_container_width=True):
@@ -35,7 +36,7 @@ if st.session_state.page == "Home":
             st.session_state.page = "Study Guides"
 
     with col2: 
-        st.subheader("Available AP Exams:", use_container_width=True)
+        st.subheader("Available AP Exams:")
 
     ap_exams = [
         "AP Chemistry",
