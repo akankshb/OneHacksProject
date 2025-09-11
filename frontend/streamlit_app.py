1751a0107f84a129a6fdc79c66b89bcb53ec7b8f
AGoel894 <aadi.goel2009@gmail.com>
2025-09-10 21:37:09 -0700

    Update streamlit_app.py

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -4,6 +4,6 @@ home_page = st.Page("homepage.py", title="Home")
 test_page = st.Page("generate_test.py", title="Practice Tests")
 study_page = st.Page("/generate_study_guide.py", title="Study Guides")
 
-pg = st.navigation([st.Page("home_page"), st.Page("test_page"), st.Page("study_page")])
+pg = st.navigation([home_page, test_page, study_page])
 
 pg.run()
