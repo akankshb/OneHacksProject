f8e3e0595a0691d45ac40f92779e6a2a4678be02
AGoel894 <aadi.goel2009@gmail.com>
2025-09-10 21:30:38 -0700

    Update streamlit_app.py

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-home_page = st.Page("pages/home.py", title="Home")
-test_page = st.Page("pages/generate_test.py", title="Practice Tests")
-study_page = st.Page("pages/generate_study_guide.py", title="Study Guides")
+home_page = st.Page("homepage.py", title="Home")
+test_page = st.Page("generate_test.py", title="Practice Tests")
+study_page = st.Page("/generate_study_guide.py", title="Study Guides")
 
 pg = st.navigation([home_page, test_page, study_page])
-
-st.set_page_config(page_title="AP Study App", page_icon="📚")
 
 pg.run()
