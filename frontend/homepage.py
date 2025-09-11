02cd1f4b540a9f79aa51c1ceedf6d86da0f00437
AGoel894 <aadi.goel2009@gmail.com>
2025-09-10 21:35:33 -0700

    Update homepage.py

--- frontend/homepage.py
+++ frontend/homepage.py
@@ -4,7 +4,7 @@ col1, col2, col3 = st.columns([1, 2, 1])
 
 col4, col5 = st.columns([1, 1])
 
-
+col6, col7, col8 = st.columns([1, 2, 1])
 with col2:
         st.title("App Name")
         st.subheader("slogan")
@@ -17,7 +17,7 @@ with col5:
     if st.button("Access Study Guides", use_container_width=True):
         st.session_state.page = "Study Guides"
 
-with col2: 
+with col7: 
     st.subheader("Available AP Exams:")
 
     ap_exams = [
